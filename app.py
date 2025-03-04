7f213f96ca023328710eff84e9dafa1848aeb4a1
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2025-03-04 21:24:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from telegram.ext import Application
+from config import TOKEN
+from handlers.user_handlers import get_user_handlers
+from handlers.admin_handlers import get_admin_handlers
 
+async def main():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handlers(get_user_handlers())
+    application.add_handlers(get_admin_handlers())
+    await application.run_polling()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
